afe42a8d4e9ff4c3752638031ec9b6e868418772
Ivan Gutierrrez <macintario@gmail.com>
2024-11-12 14:59:38 -0600

    Dirección y  justificacion en aviso de privacidad

--- resources/views/privacidad.blade.php
+++ resources/views/privacidad.blade.php
@@ -10,32 +10,29 @@
             <div class="overflow-hidden bg-white shadow-sm dark:bg-gray-800 sm:rounded-lg">
                 <div class="p-6 text-gray-900 dark:text-gray-100">
                     <h1 style="font-size:1.20em; display:block; font-weight: bold;">Aviso de Privacidad</h1><br>
-La Dirección de Educación Superior con domicilio en Av. Luis Enrique Erro S/N, Unidad Profesional Adolfo López Mateos, Zacatenco,
-Alcaldía Gustavo A. Madero, C.P. 07738, Ciudad de México, es el sujeto obligado y responsable del tratamiento de los datos personales
-que se recaban de forma general a través del sitio, los cuales serán protegidos conforme a lo dispuesto por la
-Ley General de Protección de Datos Personales en Posesión de Sujetos Obligados, y demás normatividad que resulte aplicable;
-dichos datos son los listados en el apartado siguiente. Los datos personales recolectados por las dependencias y entidades de la
- Administración Pública Federal, y empresas productivas del Estado,
+<p style="text-align: justify;"> La Dirección de Educación Superior con domicilio en Miguel Othón de Mendizabal S/N, Colonia la Escalera, Alcaldía Gustavo A. Madero, C.P. 07738, Ciudad de México, es el sujeto obligado y responsable del tratamiento de los datos personales
+que se recaban de forma general a través del sitio, los cuales serán protegidos conforme a lo dispuesto por la Ley General de Protección de Datos Personales en Posesión de Sujetos Obligados, y demás normatividad que resulte aplicable;
+dichos datos son los listados en el apartado siguiente. Los datos personales recolectados por las dependencias y entidades de la Administración Pública Federal, y empresas productivas del Estado,
  los cuales consolidan los sitios, micrositios y redes sociales a través de los portales institucionales alojados
  dentros de gob.mx, o aquellos datos recabados por cualquier otro sujeto obligado que utilice gob.mx
   para la prestación de algún servicio digital, serán tratados bajo su responsabilidad,
-  conforme a sus atribuciones legales y el aviso de privacidad correspondiente.
+  conforme a sus atribuciones legales y el aviso de privacidad correspondiente.</p>
 <br><br>
 <h2 style="font-size:1.1em; display:block; font-weight: bold;">Datos personales que se recolectan y la finalidad del tratamiento </h2>
-Los datos personales que se recaban son los necesarios para la celebración del contrato de prestación de servicios conforme
-a la normatividad vigente aplicable.
+<p style="text-align: justify;">Los datos personales que se recaban son los necesarios para la celebración del contrato de prestación de servicios conforme
+a la normatividad vigente aplicable.</p>
 <h2 style="font-size:1.1em; display:block; font-weight: bold;">Uso de Cookies</h2>
-El sitio utiliza cookies para ayudar a obtener información que nos permite mejorar la experiencia de usuario y brindar
+<p style="text-align: justify;">El sitio utiliza cookies para ayudar a obtener información que nos permite mejorar la experiencia de usuario y brindar
 un mejor servicio. En cualquier momento, dichas cookies pueden ser eliminadas accediendo a las preferencias de
-configuración del navegador en uso.
+configuración del navegador en uso.</p>
 <h2 style="font-size:1.1em; display:block; font-weight: bold;">Fundamento legal para llevar a cabo el tratamiento</h2>
-La Dirección de Educación Superior trata los datos personales antes señalados con fundamento en los artículos 6°
+<p style="text-align: justify;">La Dirección de Educación Superior trata los datos personales antes señalados con fundamento en los artículos 6°
 Base A y 16 segundo párrafo de la Constitución Política de los Estados Unidos
 Mexicanos; 3°, fracción XXXIII, 4°, 16, 17 y 18 de la Ley General de Protección de Datos Personales en Posesión de Sujetos
-Obligados; 1° y 8º, fracción I, y transitorios Segundo y Décimo Tercero de la Ley Orgánica de la Administración Pública Federal.
+Obligados; 1° y 8º, fracción I, y transitorios Segundo y Décimo Tercero de la Ley Orgánica de la Administración Pública Federal.</p>
 
 <h2 style="font-size:1.1em; display:block; font-weight: bold;">Transferencia de datos personales</h2>
-No se realizarán transferencias de datos personales, salvo aquéllas que sean necesarias para atender requerimientos debidamente fundados y motivados, provenientes de una autoridad competente.
+<p style="text-align: justify;">No se realizarán transferencias de datos personales, salvo aquéllas que sean necesarias para atender requerimientos debidamente fundados y motivados, provenientes de una autoridad competente.</p>
                 </div>
             </div>
         </div>
